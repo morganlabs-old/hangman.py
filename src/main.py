5692e164579fc7d17a92810e96dff7dfe93c2c68
Morgan Jones <me@morganlabs.dev>
2024-04-08 10:24:22 +0100

    feat: Guess letters and add them to correct_guesses in the correct position

--- src/main.py
+++ src/main.py
@@ -3,11 +3,23 @@ MAX_LIVES = 10
 
 
 class Game:
-    lives_used = 0
+    word = []
+    correct_guesses = []
     letters_guessed = []
+    lives_used = 0
+
+    def __init__(self):
+        self.word = list("somethsings")
+        self.correct_guesses = [""] * len(self.word)
 
     def get_guess(self):
         guess = input("What's your guess? ")
+        if len(guess) != 1:
+            self.handle_word_guess(guess)
+        else:
+            self.handle_letter_guess(guess)
+
+    def handle_letter_guess(self, guess):
         if guess in self.letters_guessed:
             print("That has already been guessed!")
             self.get_guess()
@@ -17,7 +29,14 @@ class Game:
             print("Please only enter letters!")
             self.get_guess()
             return
-        self.letters_guessed.append(guess)
+        self.letters_guessed.append(guess.lower())
+
+        for i in range(len(self.word)):
+            if (self.word[i] == guess):
+                self.correct_guesses[i] = guess
+
+    def handle_word_guess(self, guess):
+        print("Placeholder")
 
     def use_life(self):
         self.lives_used += 1
@@ -30,3 +49,7 @@ class Game:
 
 
 game = Game()
+print(game.word)
+print(game.correct_guesses)
+game.get_guess()
+print(game.correct_guesses)

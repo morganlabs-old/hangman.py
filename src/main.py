281842a7973d5f4638dac6c849537e342bf6967f
Morgan Jones <me@morganlabs.dev>
2024-04-08 10:11:14 +0100

    feat: Game over

--- src/main.py
+++ src/main.py
@@ -6,21 +6,27 @@ class Game:
     lives_used = 0
     letters_guessed = []
 
-    # Recieves an input from stdin
-    # Checks if it has already been guessed. If so, allow the user to retry
-    # If not, add it to self.letters_guessed
     def get_guess(self):
         guess = input("What's your guess? ")
         if guess in self.letters_guessed:
             print("That has already been guessed!")
             self.get_guess()
             return
+
+        if not guess.isalpha():
+            print("Please only enter letters!")
+            self.get_guess()
+            return
         self.letters_guessed.append(guess)
+
+    def use_life(self):
+        self.lives_used += 1
+        if self.lives_used >= MAX_LIVES:
+            self.lives_used = MAX_LIVES
+            self.game_over()
+
+    def game_over(self):
+        print("Placeholder")
 
 
 game = Game()
-game.get_guess()
-print(game.letters_guessed)
-game.get_guess()
-game.get_guess()
-print(game.letters_guessed)
